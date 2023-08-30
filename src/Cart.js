6c52ed54bcf4a3f0e51e18c388dd81d6cc37ef34
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-08-31 00:00:44 +0300

    return div of cart component

--- src/Cart.js
+++ src/Cart.js
@@ -25,4 +25,32 @@ function Cart({ cart, removeFromCart, user }) {
     })
     setTotalPrice(total)
   }
-}
+
+  return (
+    <div className="cart-container">
+      <h2 className="cart-header">Cart</h2>
+      {cart.length === 0 ? (
+        <div className="empty-cart">
+          <h3>Your cart is empty.</h3>
+        </div>
+      ) : (
+        <div>
+          <ul>
+            {cart.map((product) => (
+              <li key={product.product_name} className="cart-item">
+                <img src={product.product_full_image} alt={`${product.product_name} Full`} />
+                <div className="cart-item-details">
+                  <h3>{product.product_name}</h3>
+                  <p>{product.product_description}</p>
+                  <h4>Price: Ksh {product.unit_price}</h4>
+                  <p>Ranking: {product.ranking}</p>
+                  <div className="quantity-control">
+                    <label htmlFor={`quantity-${product.product_name}`}>Quantity:</label>
+                    <input
+                      type="number"
+                      id={`quantity-${product.product_name}`}
+                      min={1}
+                      value={quantities[product.product_name] || 1}
+                      onChange={(e) => updateQuantity(product, parseInt(e.target.value))}
+                    />
+                  </div>
