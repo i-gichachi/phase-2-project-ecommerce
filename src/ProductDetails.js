5aa440779ff62d9390bdb2776956a55cc3ea6644
+ <+>
2023-08-30 23:51:54 +0300

    corrected js file

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './ProductDetail.css'; //Import your custom CSS for styling
 
