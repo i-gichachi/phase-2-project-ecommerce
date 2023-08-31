2fb503907ed12caf042aeea2d48f61be1ef38074
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-08-31 11:13:32 +0300

    feat:POST

--- src/SignUp.js
+++ src/SignUp.js
@@ -22,3 +22,33 @@ function SignUp() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
+
+    if (!formData.agreedToTerms) {
+      alert("Please check the terms and conditions.")
+      return
+    }
+
+    if (formData.password !== formData.confirm_password) {
+      alert("Passwords do not match. Please retry again")
+      return;
+    }
+
+    fetch("https://madukani-server.onrender.com/users", {
+    method: "POST",
+    headers: {
+      "Content-Type" : "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("Registration successful. You can now sign in.")
+        window.location.href = '/signin'
+      } else {
+        alert("Registration failed! Please try again")
+      }
+    })
+    .catch((error) => {
+      console.error("Error registering user:", error)
+    })
+}
