c1d4934e98bda9eca415ed6e12161ae2f5ee8d15
robert neriko <nerikorobert@gmail.com>
2023-08-31 10:55:10 +0300

    handles form submission, fetches userdata and performs login validation with alerts

--- src/SignIn.js
+++ src/SignIn.js
@@ -16,3 +16,27 @@ const [formData, setFormData] = useState({
       [name]: value,
     }));
   }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("https://madukani-server.onrender.com/users")  
+      .then((response) => response.json())
+      .then((usersData) => {
+        const user = usersData.find(
+          (user) => user.email === formData.email && user.password === formData.password
+        );
+
+        if (user) {
+          setUser(user); 
+          handleLogin(user); 
+          navigate("/");
+          alert("Login successful");
+        } else {
+          alert("Lmao wrong email or password. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.log("Error logging in:", error);
+        alert("Error logging in. Please try again.");
+      });
+  };
