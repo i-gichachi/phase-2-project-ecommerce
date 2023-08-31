16d423efdeda6a0ccd448371b21ed63883567eb4
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-08-31 10:03:53 +0300

    Function for the Checkout component

--- src/Checkout.js
+++ src/Checkout.js
@@ -55,4 +55,54 @@ function Checkout({ cart, user, quantities, clearCart }) {
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("")
   const deliveryFee = selectedCounty.toLowerCase() === "nairobi" ? 200 : 400
   const [showReceipt, setShowReceipt] = useState(false)
+
+  const handleReceipt = () => {
+    if (!selectedCounty || !selectedPaymentMethod) {
+      alert("Please add the delivery address and select a payment method.")
+      return
+    }
+    setShowReceipt(true)
+  }
+
+
+  const calculateTotalPayment = () => {
+    const cartTotal = cart.reduce((total, product) => {
+      const quantity = quantities[product.product_name] || 1
+      return total + product.unit_price * quantity
+    }, 0)
+    return cartTotal + deliveryFee
+  }
+  
+  const generateOrderReceipt = () => {
+    const totalPayment = calculateTotalPayment()
+
+    return (
+      <div className="order-receipt">
+        <h3>Order Receipt</h3>
+        <p className="receipt-item">Delivery Address: {selectedCounty}</p>
+        <p className="receipt-item">Payment Method: {selectedPaymentMethod}</p>
+        <p className="receipt-item">Delivery Fee: Ksh {deliveryFee}</p>
+        <p className="receipt-item">Product Delivery: Within 7 business days</p>
+        <p className="receipt-item">Order Details:</p>
+        <ul className="order-details">
+          {cart.map((product) => (
+            <li key={product.product_name} className="receipt-item">
+              {product.product_name} (Quantity: {quantities[product.product_name] || 1}) - Ksh {product.unit_price}
+            </li>
+          ))}
+        </ul>
+        <p className="receipt-total">Total Payment: Ksh {totalPayment.toFixed(2)}</p>
+        <p className="receipt-message">Thank you for shopping with us and please come again!</p>
+      </div>
+        )
+    }
+
+    const handleConfirmReceipt = () => {
+    clearCart()
+    setShowReceipt(false)
+  }
+
+  if (!user) {
+    return <div className="checkout-container">You must be logged in to proceed with checkout.</div>
+  }
 }
