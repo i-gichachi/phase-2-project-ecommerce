6b4882e5b08da2b36e0a9cd5256d55262f1be8a4
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-08-31 11:41:10 +0300

    feat:complete signup.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -52,3 +52,80 @@ function SignUp() {
       console.error("Error registering user:", error)
     })
 }
+
+  return (
+    <div className="signUp-container">
+      <h2>Register</h2>
+      <form className="signUp-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="first_name">First Name:</label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            value={formData.first_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="last_name">Last Name:</label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            value={formData.last_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm_password">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm_password"
+            name="confirm_password"
+            value={formData.confirm_password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            name="agreedToTerms"
+            checked={formData.agreedToTerms}
+            onChange={handleChange}
+          />
+          <label className="checkbox-label">I agree to the terms and conditions</label>
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  )
+};
+export default SignUp;
