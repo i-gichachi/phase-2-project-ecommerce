d09d93073b1fefc4ec975178e1c04aae98de2348
robert neriko <nerikorobert@gmail.com>
2023-08-31 10:35:34 +0300

    used react hooks to manage form dataand user state updating form fields with user input

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,3 +1,18 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './SignIn.css';
+
+const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [user, setUser] = useState(null);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
