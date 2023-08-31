4f89061b3973f351f7439a194c7c46a8e4e061dc
robert neriko <nerikorobert@gmail.com>
2023-08-31 11:18:18 +0300

    rendered a login form, displayed user data upon login and provided a registration link

--- src/SignIn.js
+++ src/SignIn.js
@@ -39,4 +39,51 @@ const [formData, setFormData] = useState({
         console.log("Error logging in:", error);
         alert("Error logging in. Please try again.");
       });
-  };
+  
+
+  return (
+    <div className="signIn-container">
+      <h2>Login</h2>
+      <form className="signIn-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      
+      
+      {user && (
+        <div className="user-data">
+          <h3>Welcome, {user.name}!</h3>
+          <p>Email: {user.email}</p>
+          
+        </div>
+      )}
+      
+      <p>Don't have an account? <Link to="/signup"><button className="register-button">Register here</button></Link></p>
+    </div>
+  );
+}
+
+export default SignIn;
